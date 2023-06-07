9b6a654b57ff642fddeb852c4c8008cb82416323
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-06-07 15:57:41 +0100

    initialisation

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function NFTDropPage() {
+  return (
+    <div className="flex h-screen flex-col" >
+    {/*Left */}  
+    <div>
+        <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+            <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
+             src="https://links.papareact.com/8sg" alt="" />
+             <div>
+                <h1>PAPAFAM Apes</h1>
+                <h2>A collection of PAPAFAM Apes who live & breathe React !</h2>
+             </div>
+
+
+            
+        </div>
+    </div>
+    {/*Right */} 
+     
+    <div></div>
+    </div>
+  )
+}
+
+export default NFTDropPage
