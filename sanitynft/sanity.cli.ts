b64a7e57965f9d2e31567430d38354f2ee08b413
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-06-08 20:08:23 +0100

    setup Sanity

---
+++ sanitynft/sanity.cli.ts
@@ -0,0 +1,12 @@
+import createImageUrlBuilder from '@sanity/image-url'
+import {createCurrentUserHook, createClient} from 'next-sanity'
+
+export const config = {
+  projectId: '8863nxj0',
+  dataset: 'production',
+  apiVersion: '2023-06-08',
+  useCdn: 'production',
+}
+
+export const sanityClient = createClient(config)
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
---
+++ sanitynft/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanityNFT',
+
+  projectId: '8863nxj0',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
