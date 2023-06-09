6cea7fdad70e9a26bf6f2771a2a97b552fd4f3d0
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-06-09 12:08:46 +0100

    change cli

--- sanitynft/sanity.cli.ts
+++ sanitynft/sanity.cli.ts
@@ -5,7 +5,7 @@ export const config = {
   projectId: '8863nxj0',
   dataset: 'production',
   apiVersion: '2023-06-08',
-  useCdn: 'production',
+  // useCdn: 'production',
 }
 
 export const sanityClient = createClient(config)
